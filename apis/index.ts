9f0e3c8cd932424ada6e5dac74d1dcb4a27368da
Manjit Pardeshi <manjitpardeshi91991@gmail.com>
2022-11-02 13:03:44 +0530

    Init clients

---
+++ apis/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './GroupManagementApi';
+export * from './InstanceApi';
+export * from './MessageSendingApi';
+export * from './MiscellaneousApi';
